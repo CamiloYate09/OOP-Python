1908e3459eb14b42ee2ae6bcb13bbe5868cd5ea2
camilo <cyateya35439@universidadean.edu.co>
2018-02-04 23:55:51 -0500

    creando la clase principal

--- clases.py
+++ clases.py
@@ -1,2 +1,34 @@
+
+import time
 class Auto():
-    print()
+    color = ""
+    marca = ""
+    modelo =""
+    llantas = 0
+    velocidad = 0
+
+    def __init__(self, color, marca, modelo, llantas, velocidad):
+        self.color = color
+        self.marca = marca
+        self.modelo = modelo
+        self.llantas = llantas
+        self.velocidad = velocidad
+
+    def caracteristicas(self):
+        print("Color: ", self.color)
+        print("Marca: ", self.marca)
+        print("Modelo: ", self.modelo)
+        print("Llantas: ", self.llantas)
+        print("Velocidad: ", self.velocidad)
+    def aceleracion(self, aceleracion):
+        for i in range(0, aceleracion, 2):
+            print("Mi automil esta acelerando", i, "KM/s")
+        time.sleep(0.1)
+
+    def frenado(self, aceleracion,frenado):
+        for i in range(aceleracion,frenado,-2):
+            print("Mi automil esta frenando a ", i, "KM/s")
+        time.sleep(0.1)
+
+    def detenido(self):
+            print("Mi automil se detuvo")
